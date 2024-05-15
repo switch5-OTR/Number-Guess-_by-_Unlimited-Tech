dd89c87cdefd308eef1c5e3240a2e3425e865eaf
switch5-OTR <167744813+switch5-OTR@users.noreply.github.com>
2024-05-15 19:27:02 +0100

    Create Guess by unlimited tech.py

---
+++ Guess by unlimited tech.py
@@ -0,0 +1,31 @@
+import random
+
+# Set the number to be guessed
+number_to_guess = random.randint(1, 10)
+
+# Set the number of attempts
+attempts = 0
+
+# Set the maximum number of attempts
+max_attempts = 5
+
+# Game loop
+while attempts < max_attempts:
+    # Ask the user for their guess
+    user_guess = int(input("Guess a number between 1 and 10: "))
+
+    # Check if the user's guess is correct
+    if user_guess == number_to_guess:
+        print("Congratulations! You guessed the number correctly.")
+        break
+    elif user_guess < number_to_guess:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+
+    # Increment the number of attempts
+    attempts += 1
+
+# Check if the user ran out of attempts
+if attempts == max_attempts:
+    print("Sorry, you ran out of attempts. The number was " + str(number_to_guess))
